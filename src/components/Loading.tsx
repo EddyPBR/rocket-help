7b720040c0b2ec1696aad671085100618bdcd411
EddyPBR <edvaldojunior1310@gmail.com>
2022-07-18 23:41:29 -0300

    :sparkles: implements Loading component

---
+++ src/components/Loading.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+import { Center, Spinner } from "native-base";
+
+export const Loading: FC = () => {
+  return (
+    <Center flex={1} bg="gray.700">
+      <Spinner color="secondary.700" />
+    </Center>
+  );
+};
