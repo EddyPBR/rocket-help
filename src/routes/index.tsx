5e6e48ede03c669e1c8cc53b5c5172fedb548d07
EddyPBR <edvaldojunior1310@gmail.com>
2022-07-20 21:02:19 -0300

    :sparkles: adjusting routes when user is authenticated

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,34 @@
-import type { FC } from "react";
+import { FC, useState, useEffect } from "react";
 
 import { NavigationContainer } from "@react-navigation/native";
+import auth, { FirebaseAuthTypes } from "@react-native-firebase/auth";
 
 import { AppRoutes } from "./app.routes";
 import { SignIn } from "../screens/SignIn";
 
+import { Loading } from "../components/Loading";
+
 export const Routes: FC = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [user, setUser] = useState<FirebaseAuthTypes.User>();
+
+  useEffect(() => {
+    const subscriber = auth()
+      .onAuthStateChanged((response) => {
+        setUser(response);
+        setIsLoading(false);
+      });
+    
+    return subscriber;
+  }, []);
+
+  if (isLoading) {
+    return <Loading />
+  }
+
   return (
     <NavigationContainer>
-      <AppRoutes />
+      {user ? <AppRoutes /> : <SignIn />}
     </NavigationContainer>
   );
 };
