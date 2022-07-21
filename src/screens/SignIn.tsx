b983a7944186f8f91c38743942976e8f7e89ecd7
EddyPBR <edvaldojunior1310@gmail.com>
2022-07-20 21:01:54 -0300

    :sparkles: SignIn.screen was integrated with firebase

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,4 +1,6 @@
 import { useState, FC } from "react";
+import { Alert } from "react-native";
+import auth from "@react-native-firebase/auth";
 import { VStack, Heading, Icon, useTheme } from "native-base";
 import { Envelope, Key } from "phosphor-react-native";
 
@@ -9,11 +11,32 @@ import { Button } from "../components/Button";
 export const SignIn: FC = () => {
   const { colors } = useTheme();
 
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
 
   const handleSignIn = () => {
-    console.log(username, password);
+    if(!email || !password) {
+      return Alert.alert("Entrar", "Informe e-mail e senha");
+    }
+
+    setIsLoading(true);
+
+    auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch((error) => {
+        setIsLoading(false);
+
+        if (error.code === "auth/invalid-email") {
+          return Alert.alert("Entrar", "E-mail inválido.");
+        }
+
+        if (error.code === "auth/wrong-password" || error.code === "auth/user-not-found") {
+          return Alert.alert("Entrar", "E-mail ou senha inválida.");
+        }
+
+        return Alert.alert("Entrar", "Não foi possível acessar");
+      });
   }
 
   return (
@@ -27,7 +50,7 @@ export const SignIn: FC = () => {
       <Input
         placeholder="E-mail"
         mb={4}
-        onChangeText={setUsername}
+        onChangeText={setEmail}
         InputLeftElement={
           <Icon 
             as={<Envelope color={colors.gray[300]} />}
@@ -53,6 +76,7 @@ export const SignIn: FC = () => {
         title="Entrar"
         width="full"
         onPress={handleSignIn}
+        isLoading={isLoading}
       />
     </VStack>
   );
